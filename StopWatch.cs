7cfab16784d11180a3d1cc3db342dd16ff0509c0
Shahd El-Badrawy <91445925+Little-grow@users.noreply.github.com>
2022-05-06 17:25:16 +0200

    Add files via upload

---
+++ StopWatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class StopWatch
+    {
+        static DateTime _startTime;
+        static DateTime _stopTime;
+
+        public static DateTime Start ()
+        {
+            Console.WriteLine("our watch has started");
+            _startTime = DateTime.Now;
+            return _startTime;
+        }
+
+        public static DateTime Stop ()
+        {
+            Console.WriteLine("our watch has stopped");
+            _stopTime = DateTime.Now;
+            return _stopTime;
+        }
+
+        public static TimeSpan Duration ()
+        {
+
+            TimeSpan span = _stopTime -_startTime;
+            Console.WriteLine("The duration is : " + span);
+            return span;
+        }
+    }
+}
---
+++ System.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class System
+    {
+       static int i = 0; 
+
+        public static void Interface ()
+        {
+            Console.WriteLine("**************************************");
+            Console.WriteLine("      welcome to our StopWatch   ");
+            Console.WriteLine("      ************************    ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("press 1 to start");
+            Console.WriteLine("press 2 to stop");
+            Console.WriteLine("press 3 to display the duration");
+            Console.WriteLine("note : you can't press the the strat twice ");
+
+        }
+
+        public static void Implement()
+        {
+            while (true)
+            {
+                if (i == 0)
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (!(i == 1))
+                    { Console.WriteLine("Invalid input.Try Again");
+                        i = 0;
+                    }
+                    else
+                        StopWatch.Start();
+
+                }
+
+                if (i == 1)
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (!(i == 2))
+                    {
+                        Console.WriteLine("Invalid input .Try Again");
+                        i = 1;
+                    }
+                    else
+                        StopWatch.Stop();
+
+                }
+
+                if (i == 2)
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (!(i == 3 || i == 1))
+                    {
+                        Console.WriteLine("Invalid input .Try Again");
+                        i = 2;
+                    }
+                    else if (i == 3)
+                    {
+                        StopWatch.Duration();
+                        i = 0;
+                    }
+                    else if (i == 1)
+                        StopWatch.Start();
+                }
+
+            }
+        }
+        
+
+    }
+}
